3a5ba24c20c7ec9dd6b9f05abc8f8f217a3bb0ba
Mangla75441 <manglaverma0@gmail.com>
2018-05-07 20:19:30 +0530

    First commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController	
+
+	def index
+		@users = User.all
+	end
+
+	def edit
+		@user = User.find(params[:id])
+		@roles = Role.all
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			p "fdshfsjf"
+			redirect_to users_path
+		else
+			render 'edit'
+		end		
+	end
+
+	private
+		def user_params
+			p "-------------------"
+			params.require(:user).permit(:name,role_users_attributes: [:id, :user_id,:role_id])
+		end
+
+end	
+
+
+# role_users_attributes: [:id, :user_id,:role_id,:_destroy]
